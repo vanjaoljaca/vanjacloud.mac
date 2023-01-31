35e61ae69a5ccc170e7033aca5cd82690db73725
Vanja Oljaca <vanja.oljaca@me.com>
2023-01-31 14:37:34 -0800

    Initial Commit

---
+++ vanjacloudmac/vanjacloudmacApp.swift
@@ -0,0 +1,17 @@
+//
+//  vanjacloudmacApp.swift
+//  vanjacloudmac
+//
+//  Created by Vanja Oljaca on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct vanjacloudmacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
